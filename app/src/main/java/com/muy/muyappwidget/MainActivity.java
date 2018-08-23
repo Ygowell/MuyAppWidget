05124b430e77fef08a0a1ef37570a6c13058ee37
james <yangguowei1@xiaomi.com>
2018-08-23 18:07:54 +0800

    Add the google sample

---
+++ app/src/main/java/com/muy/muyappwidget/MainActivity.java
@@ -0,0 +1,13 @@
+package com.muy.muyappwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
